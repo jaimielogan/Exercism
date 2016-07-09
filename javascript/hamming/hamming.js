d99ef26e70ac91a3ce3c22715e37b1543401785e
jaimielogan <jaimielogan@gmail.com>
2016-07-09 15:03:19 -0600

    Updated code to include error message if inputs are of different lengths

--- javascript/hamming/hamming.js
+++ javascript/hamming/hamming.js
@@ -1,15 +1,19 @@
 
-var Hamming = function() {
-};
+var Hamming = function() {};
 
 Hamming.prototype.compute = function(input1,input2) {
   var count = 0;
-  for (i=0; i<input1.length; i++){
-	   if(input1[i] !== input2[i]){
+  if(input1.length === input2.length){
+    for (i=0; i<input1.length; i++){
+	     if(input1[i] !== input2[i]){
 		     count++;
 	      }
-    }
-  return count;
+      }
+    return count;
+  }
+  else {
+    return "The DNA strands must be of equal length"; 
+  }
 };
 
 module.exports = Hamming;
